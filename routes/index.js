c741ea3823f479c167a843875f1754fe94b42173
Jram2001 <jayaramasankaramoorthi@gmail.com>
2024-02-17 21:23:28 +0530

    Tried Some thing but nothing worked just for the green box in git :-(

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const app = express();
+const cors = require('cors');
+app.use(cors({
+  origin: "http://localhost:4200",
+}));
 
+/* Connect to Database */
 const dbConnection = require('../dbconnection');
 
 /* GET home page. */
@@ -8,14 +14,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Deliver Task Data to Frontend */ 
 router.get('/todo', (req, res) => {
-    console.log('hello');
   dbConnection.con()
     .then((connection) => {
       const query = 'select * from taskdetail';
       connection.query(query, (err, result) => {
         if (err) {
-          console.log(err);
+          console.log(err,"there is an error");
         }
         else {
           res.send(result)
